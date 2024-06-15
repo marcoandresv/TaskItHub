488a810753dee3dfc75e6cfbfb012ec97bc18f92
marcoandresv <153276680+marcoandresv@users.noreply.github.com>
2024-06-15 10:47:32 +0200

    Initial commit
    Base Spring Initializr structure build

---
+++ src/main/java/com/ironhack/taskithub/TaskithubApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.taskithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/taskithub/TaskithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.taskithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
